7e8da9749d31f0f65027d424d9ef1adeaec8205a
joao <joaopedro37@gmail.com>
2021-06-07 15:29:07 -0300

    salvo

---
+++ Projeto_de_Produtos/Classes/Marca.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Projeto_de_Produtos.Interfaces;
+
+namespace Projeto_de_Produtos.Classes
+{
+    public class Marca 
+    {
+       
+    }
+}
---
+++ Projeto_de_Produtos/Interfaces/Imarca.cs
@@ -0,0 +1,9 @@
+using Projeto_de_Produtos.Classes;
+
+namespace Projeto_de_Produtos.Interfaces
+{
+    public interface Imarca
+    {
+        
+    }
+}
